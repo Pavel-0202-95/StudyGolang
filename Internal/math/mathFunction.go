e13b21eb6e8ca29b5039ad447d7a3a163dc8b924
Pavel-0202-95 <62663204+Pavel-0202-95@users.noreply.github.com>
2022-07-19 23:15:10 +0300

    new changed two

---
+++ Internal/math/mathFunction.go
@@ -0,0 +1,9 @@
+package math
+
+func Abs(dig int) int {
+	if dig<0{
+		return dig*(-1)
+	} else {
+		return dig
+	}
+}
--- cmd/test/main.go
+++ cmd/test/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/Pavel-0202-95/StudyGolang/Internal/math"
+	uuid "github.com/nu7hatch/gouuid"
+	"log"
+)
 
 func main(){
-	fmt.Println("test")
+	fmt.Println(math.Abs(-10))
+
+	str, err := uuid.NewV4()
+
+	if err!=nil{
+		log.Panic(err)
+	}
+
+	fmt.Println(str.String())
 }
