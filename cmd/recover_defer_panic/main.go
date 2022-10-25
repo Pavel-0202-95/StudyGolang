dae20e38c650c5a32c27c597f57b551239d83ab5
sergey.miron <sergey.miron@rtlabs.ru>
2022-10-25 22:31:41 +0300

    diff struct

--- cmd/recover_defer_panic/main.go
+++ cmd/recover_defer_panic/main.go
@@ -4,10 +4,16 @@ import (
 	"fmt"
 )
 
+type DifficultData struct {
+	a   int
+	str string
+}
+
 func a() {
 	fmt.Println("Inside a()")
 	defer func() {
 		if c := recover(); c != nil {
+			fmt.Printf("Our struct: %v\n", c)
 			fmt.Println("Recover inside a()!")
 		}
 	}()
@@ -19,7 +25,11 @@ func a() {
 
 func b() {
 	fmt.Println("Inside b()")
-	panic("Panic in b()!")
+	diffStruct := DifficultData{
+		a:   10,
+		str: "test",
+	}
+	panic(diffStruct)
 	fmt.Println("Exiting b()")
 }
 
